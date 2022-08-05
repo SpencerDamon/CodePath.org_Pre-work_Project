6ae17148116e89905cde02b9b3999a810604f29c
SpencerDamon <spencerdamon1974@gmail.com>
2022-08-04 19:42:24 -0700

    Reference Button in MainActivity, set Listener, test with Log - pass

--- app/src/main/java/com/gmail/spencerdamon1974/helloworld/MainActivity.kt
+++ app/src/main/java/com/gmail/spencerdamon1974/helloworld/MainActivity.kt
@@ -2,10 +2,17 @@ package com.gmail.spencerdamon1974.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.button_intro)
+        button.setOnClickListener {
+            Log.v("Hello world", "Button clicked!")
+        }
     }
 }
